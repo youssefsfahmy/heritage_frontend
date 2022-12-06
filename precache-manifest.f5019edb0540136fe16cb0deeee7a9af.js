7b3883983b50cbe84d66c8d1703dd80511d4d701
youssefsfa7my <youssef.samir@student.guc.edu.eg>
2022-12-06 13:45:46 +0200

    Updates

---
+++ precache-manifest.f5019edb0540136fe16cb0deeee7a9af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f18b04b7ffe30a31e2f",
+    "url": "/heritage_frontend/static/css/main.d3fcec82.chunk.css"
+  },
+  {
+    "revision": "7f18b04b7ffe30a31e2f",
+    "url": "/heritage_frontend/static/js/main.7f18b04b.chunk.js"
+  },
+  {
+    "revision": "58b944829876ab6247f0",
+    "url": "/heritage_frontend/static/js/1.58b94482.chunk.js"
+  },
+  {
+    "revision": "3ad7d9332067d71c9e5d",
+    "url": "/heritage_frontend/static/css/2.c0b38006.chunk.css"
+  },
+  {
+    "revision": "3ad7d9332067d71c9e5d",
+    "url": "/heritage_frontend/static/js/2.3ad7d933.chunk.js"
+  },
+  {
+    "revision": "3c94cabc72918caede13",
+    "url": "/heritage_frontend/static/js/runtime~main.3c94cabc.js"
+  },
+  {
+    "revision": "5f441340d5c4e9d671a2789b0e1b5b5f",
+    "url": "/heritage_frontend/static/media/Qanelas-Bold.5f441340.otf"
+  },
+  {
+    "revision": "7a354e073c1f181404b76d228ced58ac",
+    "url": "/heritage_frontend/static/media/Qanelas-Medium.7a354e07.otf"
+  },
+  {
+    "revision": "130b1fe9ef3cd58a3d28def376b089d7",
+    "url": "/heritage_frontend/static/media/Qanelas-MediumItalic.130b1fe9.otf"
+  },
+  {
+    "revision": "c21b4ad1a44f174713a673ec6af0fdd9",
+    "url": "/heritage_frontend/static/media/Qanelas-Light.c21b4ad1.otf"
+  },
+  {
+    "revision": "20ef2840e9c69624d500367bce0216cc",
+    "url": "/heritage_frontend/static/media/Qanelas-BoldItalic.20ef2840.otf"
+  },
+  {
+    "revision": "963654fb8ed6231c72f6408565af9caf",
+    "url": "/heritage_frontend/index.html"
+  }
+];
